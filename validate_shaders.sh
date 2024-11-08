aa639f524ba98d10c313980bae1d915bbe68dcdd
BlackHole00 <lastapocalypse9@gmail.com>
2024-11-08 14:02:29 +0100

    Build: Allow to validate shader without building the renderer

---
+++ validate_shaders.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ ! -f build/shader_validator ]; then
+	odin build tools/shader_validator -o=aggressive -collection:shared=shared -out:build/shader_validator -collection:project=src -strict-style -vet -show-timings
+	if [[ $? -ne 0 ]]; then
+		exit 1
+	fi
+fi
+
+build/shader_validator res/shaders/renderer/obj_draw.wgsl -allow-namespaces -include-path:res/shaders
+if [[ $? -ne 0 ]]; then
+	exit 1
+fi
