680c4b7e4ec84da268a5c485d2993af578097323
BlackHole00 <lastapocalypse9@gmail.com>
2025-02-18 20:29:37 +0100

    Project: Make things build

--- build.bat
+++ build.bat
@@ -1,4 +1,2 @@
 @echo off
-call validate_shaders.bat
-
-odin build src -debug -collection:shared=shared -out:build/app.exe -strict-style -vet -show-timings -define:WGPU_DEBUG=false
+odin build src -debug -collection:shared=shared -out:build/app.exe -strict-style -vet -show-timings
--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-source ./validate_shaders.sh
-
-odin build src -debug -collection:shared=shared -out:build/app -strict-style -disallow-do -vet -show-timings -define:WGPU_DEBUG=false -internal-cached
+odin build src -debug -collection:shared=shared -out:build/app -strict-style -disallow-do -vet -show-timings -internal-cached
 if [[ $? -ne 0 ]]; then
 	exit 1
 fi
